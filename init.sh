6e52c439e921ca0083cacc0993c9c62c9082617e
bagostini <bagostini@daitangroup.com>
2015-11-09 19:39:13 -0200

    Added a step to add +clipboard support into my vim

--- init.sh
+++ init.sh
@@ -12,6 +12,8 @@ sudo apt-get install -y npm
 sudo npm update
 sudo npm install
 cd -
+sudo apt-get update
+sudo apt-get install -y vim-athena
 
 echo "@@@ You probably want to install some Syntax Checkers, so syntastic plugin can really work."
 echo "@@@ For more details: https://github.com/scrooloose/syntastic/wiki/Syntax-Checkers"
