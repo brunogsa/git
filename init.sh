20ca8208d0a1b49a26d3d2e13258d819dd501f6e
Bruno Agostini <brunogsa.92@gmail.com>
2016-05-28 17:35:50 -0300

    . Organized init.sh script

--- init.sh
+++ init.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 sudo rm -fr ~/.vim
 sudo rm -fr /root/.vim
+
 sudo ln -sf ~/vim/.ctags ~/.ctags
 sudo ln -sf ~/vim/.tern-project ~/.tern-project
 sudo ln -sf ~/vim/.vim ~/.vim
 sudo ln -sf ~/vim/.vimrc ~/.vimrc
 sudo ln -sf ~/vim/.vim /root/.vim
 sudo ln -sf ~/vim/.vimrc /root/.vimrc
+
 git submodule init
 git submodule update
-cd .vim/bundle/tern_for_vim/
+
 sudo apt-get install -y npm
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
 sudo npm update
+
+cd .vim/bundle/tern_for_vim/
 npm install
+cd -
+
 #cd .vim/bundle/YouCompleteMe/
 #git submodule update --init  --recursive
 #sudo apt-get install -y build-essential cmake
 #sudo apt-get install -y python-dev python3-dev
 #./install.py --tern-completer
-cd -
+
 sudo apt-get update
 sudo apt-get install -y vim-athena
 sudo apt-get install -y exuberant-ctags
